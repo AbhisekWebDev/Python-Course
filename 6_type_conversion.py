3f21385b34767cc0f7934a2a9da529c13bb49e95
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-01 20:02:44 +0530

    Add files via upload

---
+++ 6_type_conversion.py
@@ -0,0 +1,18 @@
+name = "Abhisek"
+
+length = len(name)
+
+print("Your name has " + str(length) + " characters \n THALA for a reason")
+
+print(int("10") + int("20"))
+
+print(10 + float("20"))
+
+num_str = "123456"
+
+print(30 + int(num_str))
+
+num1 = input("enter 1st number")
+num2 = input("enter 2nd number")
+
+print(int(num1) + int(num2))
