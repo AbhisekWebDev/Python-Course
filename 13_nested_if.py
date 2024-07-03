73ef083bcbecd8b61dfd83b8499af29905494640
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-03 11:24:45 +0530

    Add files via upload

---
+++ 13_nested_if.py
@@ -0,0 +1,27 @@
+height = int(input("Enter height : "))
+
+if height >= 3:
+    print("You can ride the jhoola")
+
+    age = int(input("Enter age : "))
+
+    if age <= 18:
+        print("Pay Rs 250")
+
+    else:
+        print("Pay Rs 500")
+else:
+    print("Cant ride")
+
+print("Bye")
+
+
+a = 60
+
+if a % 2 == 0:
+    print("Even")
+
+    if a > 30:
+        print("WOu 30 se bada h bhai noicc")
+
+print("Exit")
