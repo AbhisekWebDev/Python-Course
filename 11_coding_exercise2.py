48f61dd3566a6483afb7a41424a2d9421dca5d75
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-03 11:08:00 +0530

    Add files via upload

---
+++ 11_coding_exercise2.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age : "))
+
+years_left = 90 - age
+days_left = years_left * 365
+months_left = years_left * 12
+weeks_left = years_left * 52
+
+print(f"You have {years_left} years, {months_left} months, {weeks_left} weeks & {days_left} days left to live")
