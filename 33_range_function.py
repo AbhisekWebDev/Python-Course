df3a1ad31c76136e87ff04c8d7088544e53429ac
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-11 10:01:50 +0530

    Add files via upload

---
+++ 33_range_function.py
@@ -0,0 +1,42 @@
+a = range(5)
+
+print(a)
+print(a[0])
+
+for i in a:
+    print(i)
+
+
+b = range(2,5)
+
+print(b)
+print(b[0])
+
+for i in b:
+    print(i)
+
+
+c = range(2,20,2)
+
+print(c)
+print(b[0])
+
+for i in c:
+    print(i)
+
+
+d = range(-2,-10,-1)
+
+print(d)
+print(d[0])
+
+for i in d:
+    print(i)
+
+
+total = 0
+
+for i in range(1,101):
+    total += i
+
+print(total)
