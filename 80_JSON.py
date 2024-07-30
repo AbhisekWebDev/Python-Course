dafe6c98c683404f99360c18ca09055f5443ff0d
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-30 19:22:18 +0530

    Add files via upload

---
+++ 80_JSON.py
@@ -0,0 +1,51 @@
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+# convert into JSON:
+z = json.dumps(x)
+
+# the result is a JSON string:
+print(z)
+
+# Convert Python objects into JSON strings, and print the values:
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+# Convert a Python object containing all the legal data types:
+
+a = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(a))
+
+# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(a, indent=4, separators=(". ", " = ")))
+
+# sort the result alphabetically by keys:
+print(json.dumps(a, indent=4, sort_keys=True))
